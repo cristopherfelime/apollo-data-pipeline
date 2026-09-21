8e0cfe776e61cecdc9d4675e0d0316464286915e
Cristopher Jeremie Felime <jeremie.felime@gmail.com>
2026-09-21 12:28:38 +0800

    feat(transactions): finished core functionalities and stuff including proper docstring on the class and methods
    v1.0 - docstrings are finished, generate_transaction() was made synchronous due to actually not having to await any async coroutines innit, Faker malaysian locale stuff apparently do not exist so I had to just write my own locale, odds of fraudulent transactions are now properly evaluated to 0.5% cuz apparently Faker.boolean chance_of_getting_true is unable to evaluate floats, user_id are no longer fully randomly generated (for artemis!)

--- apollo/scrapers/transactions.py
+++ apollo/scrapers/transactions.py
@@ -2,7 +2,7 @@
     not actually a scraper but its placed here anyways
     artificial finance transaction generator using Faker and stuff
     NOTE: since this aint a scaper, TransactionGenerator will not inherit/implement BaseScraper
-    v0.1
+    v1.0 - docstrings are finished, generate_transaction() was made synchronous due to actually not having to await any async coroutines innit, Faker malaysian locale stuff apparently do not exist so I had to just write my own locale, odds of fraudulent transactions are now properly evaluated to 0.5% cuz apparently Faker.boolean chance_of_getting_true is unable to evaluate floats, user_id are no longer fully randomly generated (for artemis!)
 """
 
 import logging
@@ -11,7 +11,7 @@ import random
 from faker import Faker
 from decimal import Decimal
 from datetime import datetime, timezone
-from uuid import uuid4
+from uuid import UUID, uuid4
 from collections.abc import AsyncIterator # apparently this is used for async generators like stream_transactions()
 
 from apollo.schemas import TransactionPayload
@@ -21,21 +21,136 @@ logger = logging.getLogger(__name__)
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 """
-    class placeholder docstring
+    the transaction generator class
+    attributes:
+        fake (Faker): Faker instance configured with localized Malaysian English (en_MS) provider
+        MALAYSIAN_MERCHANTS (dict[str, list[str]]): mapping of 4-digit ISO MCC codes to authentic Malaysian merchant brands
+    methods:
+        __init__ -> initializes the generator with localized (or at least malaysian companies names in english or whatever) Faker instance
+        generate_transaction -> generates a single synthetic TransactionPayload with weighted payment methods and status
+        stream_transactions -> asynchronously streams generated transaction payloads using an async generator
 """
 class TransactionGenerator:
     fake: Faker
+    user_pool: list[UUID] = [uuid4() for _ in range(1000)] # rather than randomly generating unique uuid for every user in transaction, we can use 1k generated uuid instead so that artemis may be able to learn each user patterns
+    MALAYSIAN_MERCHANTS: dict[str, list[str]] = {
+        "5411": [  # grocery stores and supermarket
+            "99 Speedmart",
+            "Jaya Grocer",
+            "Village Grocer",
+            "Lotus's Malaysia", # ts goated btw
+            "Econsave",
+            "Mydin",
+            "HeroMarket",
+            "Giant Hypermarket",
+            "NSK Trade City",
+        ],
+        "5814": [  # fnb
+            "ZUS Coffee",
+            "Tealive",
+            "Gigi Coffee",
+            "Marrybrown",
+            "Richiamo Coffee",
+            "The Alley Malaysia",
+            "KyoChon 1991 Malaysia",
+            "Rotiboy",
+        ],
+        "5812": [  # restaurants or warung
+            "Secret Recipe",
+            "The Chicken Rice Shop",
+            "Nando's Malaysia",
+            "Sushi King Malaysia",
+            "OldTown White Coffee",
+            "PappaRich",
+            "Oriental Kopi",
+            "Madam Kwan's",
+        ],
+        "5499": [  # convenience store
+            "FamilyMart Malaysia",
+            "CU Malaysia",
+            "7-Eleven Malaysia",
+            "myNEWS",
+            "emart24 Malaysia",
+        ],
+        "5541": [  # services or petrol station
+            "Petronas",
+            "Shell Malaysia",
+            "Petron Malaysia",
+            "Caltex Malaysia",
+            "BHPetrol",
+        ],
+        "5912": [  # drug stores and pharmacies
+            "Watsons Malaysia",
+            "Guardian Malaysia",
+            "Caring Pharmacy",
+            "Big Pharmacy",
+            "Alpro Pharmacy",
+        ],
+        "5311": [  # department stores
+            "AEON Malaysia",
+            "Parkson",
+            "SOGO Malaysia",
+            "Metrojaya",
+        ],
+        "5651": [  # family clothing and fashion stores
+            "Padini Concept Store",
+            "Brands Outlet",
+            "Bonia",
+            "Carlo Rino",
+            "British India",
+        ],
+        "5331": [  # variety stores and retail
+            "MR. D.I.Y.",
+            "MR. Dollar",
+            "Daiso Malaysia",
+            "Miniso Malaysia",
+        ],
+        "5732": [  # electronics
+            "All IT Hypermarket",
+            "Senheng",
+            "Thunder Match Technology (TMT)",
+            "Harvey Norman Malaysia",
+        ],
+        "4814": [  # telecommunications
+            "Maxis",
+            "CelcomDigi",
+            "U Mobile",
+            "Unifi Mobile",
+            "Yes 5G",
+        ],
+        "4900": [  # utilities including electric, water, and sewerage
+            "Tenaga Nasional Berhad",
+            "Pengurusan Air Selangor",
+            "Indah Water Konsortium",
+            "Sabah Electricity",
+            "Sarawak Energy",
+        ],
+        "4121": [  # taxicabs and ride hailing services
+            "Grab Malaysia",
+            "Maxim Malaysia",
+            "AirAsia Ride",
+        ],
+        "7832": [  # movie theatres
+            "Golden Screen Cinemas (GSC)",
+            "TGV Cinemas",
+            "MBO Cinemas",
+        ],
+    }
 
     """
-        method docstring placeholder
+        initializes the transaction generator
+        arguments: self
+        EXPECTED TO return: None
     """
     def __init__(self) -> None:
-        self.fake = Faker("en_MS")
+        self.fake = Faker()
 
     """
-        method docstring placeholder
+        generates a single fake financial transaction payload, this serves as the core method of the generator
+        arguments: self
+        EXPECTED TO return: TransactionPayload
     """
-    async def generate_transaction(self) -> TransactionPayload:
+    def generate_transaction(self) -> TransactionPayload:
         r"""
             transaction_id: Annotated[UUID, Field(default_factory=uuid4)]
             timestamp: Annotated[datetime, Field(description="exact UTC timestamp of when transaction was conducted")] # yyeeee
@@ -59,11 +174,11 @@ class TransactionGenerator:
             right_digits=2,
             positive=True
         ) # returns a Decimal object, 4 digits to the left of decimal and 2 digits to the right (so 2 decimal places, example: 9999.99), always positive too
-        user_id = uuid4()
-        merchant_name = self.fake.company() # returns a random company name as a string, since we localized them above as ms_MY, it will actually return a fake malaysian style-ish or whatever the word is company
-        merchant_mcc = self.fake.bothify(text="####") # bothify is useful to generate random set of number according to the hashtags in the given tags, so ts useful to mimic MCC
+        user_id = random.choice(self.user_pool)
+        merchant_mcc = random.choice(list(self.MALAYSIAN_MERCHANTS.keys())) # randomly select an MCC category key from our Malaysian merchants dictionary above, convert dict_keys to list then pick randomly
+        merchant_name = random.choice(self.MALAYSIAN_MERCHANTS[merchant_mcc]) # pick a real Malaysian merchant matching that exact MCC, from the selected mcc key above it will randomly pick from the list of merchant names
         payment_status = ["SUCCESS", "FAILED", "PENDING", "REVERSED"]
-        is_flagged_fraud = self.fake.boolean(chance_of_getting_true=5) # kinda self-explanatory
+        is_flagged_fraud = random.random() < 0.005 # returns True if the random number between 0.0 and 1.0 returns less than 0.005, which should properly simulates 0.5% fraudulent transactions rate
 
         return TransactionPayload(
             timestamp=timestamp,
@@ -85,30 +200,33 @@ class TransactionGenerator:
         )
     
     """
-        method placeholder docstring
+        asynchronously streams generated transaction payloads using an async generator
+        arguments: self, count (int: number of transactions to generate and stream), delay (float: delay between each generated transaction in seconds, default is 0.01)
+        EXPECTED TO return: AsyncIterator of TransactionPayload
     """
     async def stream_transactions(self, count: int, delay: float=0.01) -> AsyncIterator[TransactionPayload]:
         for _ in range(count):
-            yield await self.generate_transaction()
+            yield self.generate_transaction()
             await asyncio.sleep(delay)
 
 
 
 """
-    function placeholder docstring
+    main function just for testing directly in the terminal
 """
 async def main() -> None:
     generator = TransactionGenerator()
     
-    tx = await generator.generate_transaction()
+    tx = generator.generate_transaction()
     print(f"test generating a fake transaction:\n {tx}")
-    print(f"and its json dump (see if PlainSerializer works):\n {tx.model_dump(mode='json')}")
+    print(f"and its json dump (see if PlainSerializer works):\n {tx.model_dump(mode='json')}\n")
 
-    print("="*100) # line shi
+    print("\n" + "=" * 100 + "\n") # line shi
 
     count = 5
     async for tx_stream in generator.stream_transactions(count=count, delay=0.1):
         print(f"test streaming transactions:\n {tx_stream}")
+        print(f"their json dumps:\n {tx_stream.model_dump(mode='json')}\n")
     
 
 # ----------------------------------------------------------------------------------------------------------------------------------------------------------
